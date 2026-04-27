9a8dd68361efb4515b3ec747f73135d208db3be5
Taliesin <taliesin.bowes@hotmail.co.uk>
2026-04-27 14:47:16 +0100

    created button

---
+++ src/components/BurgerButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button>
+    <svg focusable="false" viewBox="0 0 24 24">
+      <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+    </svg>
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+button {
+  padding: 12px;
+  display: inline-block;
+  border-radius: 50%;
+  vertical-align: middle;
+  overflow: hidden;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+button:hover {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+button svg {
+  fill: currentColor;
+  color: black;
+  width: 24px;
+  height: 24px;
+}
+</style>
--- src/templates/main-template.vue
+++ src/templates/main-template.vue
@@ -1,7 +1,11 @@
-<template>
-  <div><slot /></div>
-</template>
+<script setup lang="ts">
+import BurgerButton from '@/components/BurgerButton.vue'
+</script>
 
-<script setup lang="ts"></script>
+<template>
+  <header>
+    <BurgerButton />
+  </header>
+</template>
 
 <style scoped></style>
