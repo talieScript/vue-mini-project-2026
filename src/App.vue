5cea89f8fa7dbd2b802d51e25b6a8dd85d3baefe
Taliesin <taliesin.bowes@hotmail.co.uk>
2026-04-27 14:32:25 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p class="paragraph">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped>
+.paragraph {
+  font-size: 1.2em;
+  color: red;
+}
+</style>
