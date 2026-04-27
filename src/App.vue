7b6f39ce88fe64cd2a579fae325e836c300f31ff
Taliesin <taliesin.bowes@hotmail.co.uk>
2026-04-27 14:35:53 +0100

    created new template component and added it to app component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainTemplate from './templates/main-template.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p class="paragraph">
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <MainTemplate> here </MainTemplate>
 </template>
 
 <style scoped>
